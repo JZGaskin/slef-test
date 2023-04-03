8b63e95b367a81f61040b7a1c9b6155533d41b47
jzgaskin <jzgaskin@gmail.com>
2023-04-03 09:01:53 -0400

    pages to pages

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ document.addEventListener("DOMContentLoaded", function () {
         navLinks.classList.toggle("active");
     });
 });
+
+// const route = (e) => {
+//     e = e || window.e;
+//     e.preventDefault();
+//     window.history.pushState({}, "",e.target.href);
+// }
+
+// const routes = {
+//     "/": "/index.html",
+//     "/about": "/about.html",
+//     "/register": "/register.html",
+//     "/fields": "/fields.html",
+//     "/contact": "/contact.html",
+//     "/thankyou": "/thankyou.html"
+// }
+
+// const handleLocation = async () => {
+//     const path = window.location.pathname;
+//     const route = routes[path] || routes[404];
+//     const html = await fetch(route).then((data) => data.text());
+//     document.getElementById("main-content").innerHTML = html
+// }
+
+// window.onpopstate = handleLocation;
+// window.route = route;
+
+// handleLocation();
